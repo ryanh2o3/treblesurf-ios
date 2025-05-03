e7b9552364b5a5b54afe95a02aa1385f3f32df5e
Ryan Patton <ryancpatton0@gmail.com>
2025-05-03 19:42:45 +0100

    Initial Commit

---
+++ TrebleSurf/TrebleSurfApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrebleSurfApp.swift
+//  TrebleSurf
+//
+//  Created by Ryan Patton on 03/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct TrebleSurfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TrebleSurfTests/TrebleSurfTests.swift
@@ -0,0 +1,17 @@
+//
+//  TrebleSurfTests.swift
+//  TrebleSurfTests
+//
+//  Created by Ryan Patton on 03/05/2025.
+//
+
+import Testing
+@testable import TrebleSurf
+
+struct TrebleSurfTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
